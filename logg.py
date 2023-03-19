dcef2722de8886c61b2cebacb72444e7484dd5ab
BorAlex <alexander44@bk.ru>
2023-03-19 16:24:38 +0300

    add logging

---
+++ logg.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="my_log.log",
+    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s - %(message)s",
+    datefmt='%d-%m-%Y %H:%M:%S',
+)
--- user_interface.py
+++ user_interface.py
@@ -1,27 +1,32 @@
 import keyboard
+from logg import logging
 
 
 def start_menu():
     while True:
-        print("Это приложение для работы с заметками !!!")
+        logging.info("Главное меню")
+        print("Главное меню !!!")
         type_numbers = input("Выберите действие из списка\n"
-                             "1 - Начало работы\n"
+                             "1 - Операции\n"
                              "2 - Выход\n"
                              ).strip()
         match type_numbers:
             case "1":
+                logging.info("Вход в меню Операции")
                 menu_notes(type_numbers)
             case "2":
+                logging.info("Окончание работы")
                 print("Досвидания!!!")
                 exit()
             case _:
+                logging.info("Некорректый ввод")
                 print("Вы выбрали неправилиную команду!!!! Повторите ввод!")
 
 
 def menu_notes(type_numbers):
     while True:
         if type_numbers == "1":
-            print("Меню приложения")
+            print("Меню операций")
             type_operation = input("Выберите операцию\n"
                                    "1 - Добавление новой заметки\n"
                                    "2 - Показать все заметки\n"
@@ -33,26 +38,34 @@ def menu_notes(type_numbers):
                                    ).strip()
             match type_operation:
                 case "1":
+                    logging.info("Выбор операции - Добавление новой заметки")
                     print("Для продолжения нажмите пробел!!!")
                     pause()
                 case "2":
+                    logging.info("Выбор операции - Показать все заметки")
                     print("Для продолжения нажмите пробел!!!")
                     pause()
                 case "3":
+                    logging.info("Выбор операции - Удаление заметки")
                     print("Для продолжения нажмите пробел!!!")
                     pause()
                 case "4":
+                    logging.info("Выбор операции - Редактирование заметки")
                     print("Для продолжения нажмите пробел!!!")
                     pause()
                 case "5":
+                    logging.info("Выбор операции - Поиск заметки по дате")
                     print("Для продолжения нажмите пробел!!!")
                     pause()
                 case "6":
+                    logging.info("Выбор операции - Поиск заметки по номеру")
                     print("Для продолжения нажмите пробел!!!")
                     pause()
                 case "0":
+                    logging.info("Выход в главное меню")
                     break
                 case _:
+                    logging.info("Некорректый ввод")
                     print("Неправильный выбор!!! Повторите выбор!!!")
 
 

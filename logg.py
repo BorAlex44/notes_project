a003e5bdbd30f793cfc91417fea324c62a97bf96
BorAlex <alexander44@bk.ru>
2023-03-22 21:53:28 +0300

    relise programm

--- logg.py
+++ logg.py
@@ -5,4 +5,5 @@ logging.basicConfig(
     filename="my_log.log",
     format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s - %(message)s",
     datefmt='%d-%m-%Y %H:%M:%S',
+    encoding='utf-8',
 )
